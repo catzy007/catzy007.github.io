0f4d896c3e9b6e3c36e4fd248ae6eacf323a43a3
catzy007 <catzy.rais@gmail.com>
2019-07-04 16:07:59 +0700

    update logif2

---
+++ logif2/js/predicate.js
@@ -0,0 +1,58 @@
+		function myParser(str){
+			if(KuantorCek(str) !== '0x000') { //============== jika input berupa kuantor (semua x, beberapa x)
+				return KuantorCek(str);
+			}else if(PenghubungCek(str) !== '0x000') { //===== jika input berupa (maka, dan, atau)
+				return PenghubungCek(str);
+			}else{
+				return BuatPredikat(str); //=================== misal (budi bapak ani --- bapak(budi,ani))
+			}
+		}
+		
+		function KuantorCek(str){
+			var output;
+			if(globalValidator(str,'semua ') //=============== jika input terdapat kata semua, setiap, tidak ada (kuantor umum)
+			|| globalValidator(str,'setiap ') 
+			|| globalValidator(str,'tidak ada ')){
+				output = globalGetRight(str,'semua '); //===== ambil variabel di sebelah kuantor
+				output = globalGetRight(output,'setiap ');
+				output = globalGetRight(output,'tidak ada ');
+				output = globalRemover(output,' dimana');
+				return '\u2200' + output + '.'; //============ return special character + variabel
+			}else if(globalValidator(str,'beberapa ') //====== jika input terdapat kata beberapa, paling sedikit, ada (kuantor khusus)
+			|| globalValidator(str,'paling sedikit ')
+			|| globalValidator(str,'ada ')){
+				output = globalGetRight(str,'beberapa '); //== ambil variabel di sebelah kuantor
+				output = globalGetRight(output,'paling sedikit ');
+				output = globalGetRight(output,'ada satu ');
+				output = globalGetRight(output,'ada ');
+				output = globalRemover(output,' dimana');
+				return '\u2203' + output + '.'; //============ return special character + variabel
+			}
+			return '0x000'; //================================ return 0x000 jika tidak valid
+		}
+		
+		function PenghubungCek(str){
+			if(globalValidator(str,'maka') //================= jika input berupa maka
+			&& str.length === 'maka'.length){
+				return '\u2192'; //=========================== return special character
+			}else if(globalValidator(str,'dan') //============ jika input berupa dan
+			&& str.length === 'dan'.length){
+				return '\u2227'; //=========================== return special character
+			}else if(globalValidator(str,'atau') //=========== jika input berupa atau
+			&& str.length === 'atau'.length){
+				return '\u2228'; //=========================== return special character
+			}
+			return '0x000'; //================================ return 0x000 jika tidak valid
+		}
+
+		function BuatPredikat(str){ //========================= misal (budi bapak ani --- bapak(budi, ani)
+			str = globalRemover(str,' adalah'); //============= hilangkan kata adalah pada str
+			var array = str.split('\ ');
+			var count = array.length;
+
+			if(count == 2){
+				return array[1] + '(' + array[0] + ')';
+			}else if(count == 3){
+				return array[1] + '(' + array[0] + ',' + array[2] + ')';
+			}
+		}
