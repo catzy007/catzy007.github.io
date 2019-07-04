e5d2003429591158602fa2309801b25095eb83eb
catzy007 <catzy.rais@gmail.com>
2019-07-04 20:16:39 +0700

    update logif2

--- logif2/js/predicate.js
+++ logif2/js/predicate.js
@@ -10,7 +10,7 @@
 		
 		function KuantorCek(str){
 			var output;
-			if(globalValidator(str,'semua ') //=============== jika input terdapat kata semua, setiap, tidak ada (kuantor umum)
+			if(globalValidator(str,'semua ') //=============== jika input terdapat kata [semua, setiap, tidak ada] (kuantor umum)
 			|| globalValidator(str,'setiap ') 
 			|| globalValidator(str,'tidak ada ')){
 				output = globalGetRight(str,'semua '); //===== ambil variabel di sebelah kuantor
@@ -18,10 +18,10 @@
 				output = globalGetRight(output,'tidak ada ');
 				output = globalRemover(output,' dimana');
 				return '\u2200' + output + '.'; //============ return special character + variabel
-			}else if(globalValidator(str,'beberapa ') //====== jika input terdapat kata beberapa, paling sedikit, ada (kuantor khusus)
+			}else if(globalValidator(str,'beberapa ') //====== jika input terdapat kata [beberapa, paling sedikit, ada] (kuantor khusus)
 			|| globalValidator(str,'paling sedikit ')
 			|| globalValidator(str,'ada ')){
-				output = globalGetRight(str,'beberapa '); //== ambil variabel di sebelah kuantor
+				output = globalGetRight(str,'beberapa '); //== ambil variabel di sebelah kanan kuantor
 				output = globalGetRight(output,'paling sedikit ');
 				output = globalGetRight(output,'ada satu ');
 				output = globalGetRight(output,'ada ');
@@ -45,14 +45,16 @@
 			return '0x000'; //================================ return 0x000 jika tidak valid
 		}
 
-		function BuatPredikat(str){ //========================= misal (budi bapak ani --- bapak(budi, ani)
-			str = globalRemover(str,' adalah'); //============= hilangkan kata adalah pada str
-			var array = str.split('\ ');
-			var count = array.length;
-
-			if(count == 2){
-				return array[1] + '(' + array[0] + ')';
-			}else if(count == 3){
-				return array[1] + '(' + array[0] + ',' + array[2] + ')';
+		function BuatPredikat(str){ //======================== misal (budi bapak ani --- bapak(budi, ani)
+			str = globalRemover(str,' adalah'); //============ hilangkan kata adalah pada str
+			var array = str.split('\ '); //=================== ubah kalimat menjadi array of kata
+			var output = '';
+			if(array.length > 1){ //========================== jika kata lebih dari satu
+				output = array[1] + '(' + array[0]; //======== output = predikat(term1, term2, ... )
+				for(var i=2; i<array.length; i++){
+					output += ',' + array[i];
+				}
+				output += ')';
+				return output;
 			}
 		}
