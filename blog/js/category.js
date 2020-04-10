ffcec26b80f7b5763426d9e4b95f7e1941e5f42d
catzy007 <catzy.rais@gmail.com>
2020-04-10 19:04:15 +0700

    update category p1

--- blog/js/category.js
+++ blog/js/category.js
@@ -1,5 +1,4 @@
-// console.log("test");
-// executeXhr("./kegiatan.md", getCategoryArray, "PAGE-INDEX");
+var category = "## Category\n";
 
 function getCategoryArray(){
     var text = this.responseText;
@@ -14,17 +13,37 @@ function getCategoryArray(){
     arrPosts.reverse(); 
 //filter array (remove empty value)
     var arrPosts = arrPosts.filter(function(el) { return el; });
-    var post="## Category\n";
+    for(var i=0; i<arrPosts.length; i++){
+        executeXhr("./pages/category/"+arrPosts[i]+".md", getSubcategoryArray, "PAGE-INDEX", arrPure[i]);
+        //console.log(i + " " + arrPosts[i] + " " + arrPure[i]);
+    }
+    // console.log(category);
+    // showMarkdown("PAGE", ["#!", "#!", category]);
+}
+
+function getSubcategoryArray(){
+    var text = this.responseText;
+    category += "### "+this.arguments[1]+"\n";
+//filter text from space
+    text = text.split(' ').join('');
+//store original data
+    var arrPure = text.split("\n");
+    arrPure.reverse(); 
+//store processed data
+    text = text.toLowerCase();
+    var arrPosts = text.split("\n");
+    arrPosts.reverse(); 
+//filter array (remove empty value)
+    var arrPosts = arrPosts.filter(function(el) { return el; });
     for(var i=0; i<arrPosts.length; i++){
         if(arrPure[i].length == "1" && arrPure[i] == "-"){
             arrPure.splice(i,1);
-            post=post+"<br>";
+            post=post+"<br>\n";
         }
-        post=post + "<a style=\"font-size:18px;\" onclick=\"location.href='#!"+ 
-        arrPosts[i] +"';refreshed()\">"+ arrPure[i].split('-').join(' ') +"</a>\n<br>";
+        category=category + "<a style=\"font-size:18px;\" onclick=\"location.href='#!"+ 
+        arrPosts[i] +"';refreshed()\">"+ arrPure[i].split('-').join(' ') +"</a>\n<br>\n";
         //console.log(i + " " + arrPosts[i] + " " + arrPure[i]);
     }
-    showMarkdown("PAGE", ["#!", "#!", post])
-    console.log(post);
-    showMarkdown("PAGE", ["#!", "#!", post])
+    console.log(category);
+    showMarkdown("PAGE", ["#!", "#!", category]);
 }
--- blog/pages/category/index.md
+++ blog/pages/category/index.md
@@ -1,1 +1,2 @@
-#Hello
+Kegiatan
+Lomba
