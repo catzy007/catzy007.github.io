0bf9b6780c867b47c478c92a55b18adbeaafcfe6
catzy007 <catzy.rais@gmail.com>
2022-01-23 14:49:15 +0700

    refactor blog

--- js/content-engine/content-loader.js
+++ js/content-engine/content-loader.js
@@ -18,7 +18,9 @@ function loadContentIndex(){
         }else if(typeRequest[1] == "index"){
             loadPageIndex(arrIndex, arrLower, urlRequest, pageRequest[1]);
         }else if(typeRequest[1] == "category"){
-            console.log("category");
+            loadCategoryPage(urlRequest, pageRequest[1]);
+        }else if(typeRequest[1] == "search"){
+            console.log("search");
         }else{
             document.location.href = './';
         }
@@ -26,87 +28,5 @@ function loadContentIndex(){
         loadContentRecommended(arrIndex, arrLower);
     }else{
         document.location.href = './';
-    }
-}
-
-function loadContentCategory(){
-    var categoryList = "";
-    var text = this.responseText;
-    var arrCategory = parseIndexArray(text)
-    var arrCategoryL = parseIndexLower(text)
-    for(var i=0; i<arrCategory.length; i++){
-        if(arrCategory[i].length == "1" && arrCategory[i] == "-"){
-            arrCategory.splice(i,1);
-        }
-        categoryList+= "<p class=\"clickable\"" +
-                    "onclick=\"location.href='./loader.html?category=" + 
-                    arrCategoryL[i] +"'\">" + 
-                    arrCategory[i] + "</p>\n";
-        // console.log(i + " " + arrCategory[i] + " " + arrCategoryL[i]);
-    }
-    document.getElementById("categoryList").innerHTML = categoryList;
-    document.getElementById("categoryList").style.display = 'block';
-}
-
-function loadContentPost(urlRequest, pageRequest){
-    var contentPath = "./posts/"+pageRequest+"/index.md";
-    document.getElementById("featuredPostImg").src = checkImgExist("./posts/" + pageRequest + "/thumbnail.jpg");
-    setSiteIdentifier(pageRequest, urlRequest, capitalize(getTitleOnly(pageRequest)), "en-us");
-    reqParseMarkdown("POST", contentPath);
-}
-
-function loadContentPages(urlRequest, pageRequest){
-    var contentPath = "./pages/"+pageRequest+"/index.md";
-    document.getElementById("featuredPostImg").src = checkImgExist("./pages/" + pageRequest + "/thumbnail.jpg");
-    setSiteIdentifier(pageRequest, urlRequest, capitalize(pageRequest), "en-us");
-    reqParseMarkdown("PAGES", contentPath);
-}
-
-function loadPageIndex(arrIndex, arrLower, urlRequest, pageRequest){
-    var pageIndex = "<h3>Index</h3>";
-    document.getElementById("featuredPostImg").src = checkImgExist("./pages/" + pageRequest + "/thumbnail.jpg");
-    setSiteIdentifier(pageRequest, urlRequest, capitalize(pageRequest), "en-us");
-    for(var i=0; i<arrIndex.length; i++){
-        pageIndex = pageIndex.concat("<a href='./loader.html?post="+ arrLower[i] +"'>");
-        pageIndex = pageIndex.concat("<p>"+getTitleDate(arrIndex[i])+" - "+getTitleOnly(arrIndex[i])+"</p>");
-        pageIndex = pageIndex.concat("</a>");
-    }
-    document.getElementById('main-content').innerHTML = pageIndex;
-}
-
-function loadContentRecommended(arrIndex, arrLower){
-    var contPostCardId = []; var contPostImgId = [];
-    var contPostDateId = []; var contPostTitleId = [];
-    var recommendedContentI = []; var recommendedContentL = [];
-    var contLoadMax; var dice;
-
-    if(arrIndex.length > 0){
-        if(arrIndex.length > 3){
-            contLoadMax = 3;
-        }else{
-            contLoadMax = arrIndex.length;
-        }
-        document.getElementById("recommendedContent").style.display = 'block';
-    }
-
-    for(var i=0; i<contLoadMax; i++){
-        dice = Math.floor(Math.random() * arrIndex.length);
-        recommendedContentI.push(arrIndex[dice]); 
-        recommendedContentL.push(arrLower[dice]);
-    }
-    
-    for(var i=1; i<contLoadMax+1; i++){
-        contPostCardId.push("recommendedContentCard"+i.toString());
-        contPostImgId.push("recommendedContentImg"+i.toString());
-        contPostDateId.push("recommendedContentDate"+i.toString());
-        contPostTitleId.push("recommendedContentTitle"+i.toString());
-    }
-
-    for(var i=0; i<contLoadMax; i++){
-        document.getElementById(contPostCardId[i]).href = "./loader.html?post=" + recommendedContentL[i];
-        document.getElementById(contPostImgId[i]).src = checkImgExist("./posts/" + recommendedContentL[i] + "/thumbnail.jpg");
-        document.getElementById(contPostDateId[i]).innerHTML = getTitleDate(recommendedContentI[i]);
-        document.getElementById(contPostTitleId[i]).innerHTML = getTitleOnly(recommendedContentI[i]);
-        document.getElementById(contPostCardId[i]).style.display = 'block';
     }
 }
--- js/main-engine/markdown-parser.js
+++ js/main-engine/markdown-parser.js
@@ -10,28 +10,31 @@
 		var html = converter.makeHtml(markdown);
 		document.getElementById('main-content').innerHTML = html;
 
-		console.log(getSiteIdentifier()[2]);
+		// console.log(getSiteIdentifier()[2]);
 		if(getSiteIdentifier()[2]){
 			document.title = getSiteIdentifier()[2];
 		}
 
-	//enable comment on post
+	//enable comments on post
 		if(type == "POST" && enableComments()){
-			document.getElementById('contentComments').style.display = 'block';
-
-			/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-			var disqus_shortname = getDisqusShortname();
-			var disqus_identifier = getSiteIdentifier()[0];
-			var disqus_url = getSiteIdentifier()[1];
-			var disqus_config = function () { 
-				this.language = getSiteIdentifier()[3];
-			};
-
-			/* * * DON'T EDIT BELOW THIS LINE * * */
-			(function() {
-				var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-				dsq.src = 'https://' + disqus_shortname + '.disqus.com/embed.js';
-				(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-			})();
+			loadComments();
 		}
+	}
+
+//load disqus comments
+	function loadComments(){
+		document.getElementById('contentComments').style.display = 'block';
+		/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+		var disqus_shortname = getDisqusShortname();
+		var disqus_identifier = getSiteIdentifier()[0];
+		var disqus_url = getSiteIdentifier()[1];
+		var disqus_config = function () { 
+			this.language = getSiteIdentifier()[3];
+		};
+		/* * * DON'T EDIT BELOW THIS LINE * * */
+		(function() {
+			var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+			dsq.src = 'https://' + disqus_shortname + '.disqus.com/embed.js';
+			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+		})();
 	}
