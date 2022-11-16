6d17a02f0cc36f3e05fac91dbde98d92e548f810
Bagus Saputra <38147269+catzy007@users.noreply.github.com>
2022-11-16 20:14:23 +0700

    try DOM element wrapper

--- js/content-engine/content-category.js
+++ js/content-engine/content-category.js
@@ -1,39 +1,50 @@
 //category list
+function categoryListWrapper(targetUrl, targetName){
+    var categoryElmtPara = document.createElement("p");
+    var categoryElmtAncr = document.createElement("a");
+    categoryElmtAncr.innerHTML = targetName;
+    categoryElmtAncr.href = targetUrl;
+    categoryElmtAncr.classList.add("category-list")
+    categoryElmtPara.appendChild(categoryElmtAncr);
+    return categoryElmtPara;
+}
+
 function loadCategoryList(){
-    var categoryList = "";
     var text = this.responseText;
     var arrCategory = parseIndexArray(text);
     var arrCategoryL = parseIndexLower(text);
+
+    var categoryList = document.getElementById("categoryList");
+    categoryList.innerHTML = "";
+
     for(var i=0; i<arrCategory.length; i++){
         if(arrCategory[i].length == "1" && arrCategory[i] == "-"){
             arrCategory.splice(i,1);
         }
-        categoryList = categoryList.concat("<p class='clickable' ");
-        categoryList = categoryList.concat("onclick='location.href=\"");
-        categoryList = categoryList.concat("./loader.html?category=");
-        categoryList = categoryList.concat(arrCategoryL[i] + "\"'>");
-        categoryList = categoryList.concat(arrCategory[i] + "</p>\n");
-        // console.log(i + " " + arrCategory[i] + " " + arrCategoryL[i]);
+        categoryList.appendChild(
+            categoryListWrapper(
+                arrCategoryL[i],
+                arrCategory[i]
+            )
+        );
     }
-    document.getElementById("categoryList").innerHTML = categoryList;
-    document.getElementById("categoryList").style.display = 'block';
+    categoryList.style.display = 'block';
 }
-
 
 //category page
 function loadCategoryPage(urlRequest, pageRequest){
     setSiteIdentifier(pageRequest, urlRequest, capitalize(pageRequest), "en-us");
     document.title = getSiteIdentifier()[2];
-    executeXhr("./pages/category/"+pageRequest+".md", parseCategoryPage, "CATEGORY", pageRequest);
+    executeXhr("./pages/category/" + pageRequest + ".md", parseCategoryPage, "CATEGORY", pageRequest);
 }
 
-function CategoryPageWrapper(targetUrl, targetUrlSection, targetDate, targetName){
-    var IndexElmtPara = document.createElement("p");
-    var IndexElmtAncr = document.createElement("a");
-    IndexElmtAncr.innerHTML = targetDate + targetName;
-    IndexElmtAncr.href = "./loader.html?"+ targetUrlSection + "=" + targetUrl;
-    IndexElmtPara.appendChild(IndexElmtAncr);
-    return IndexElmtPara;
+function categoryPageWrapper(targetUrl, targetUrlSection, targetDate, targetName){
+    var categoryElmtPara = document.createElement("p");
+    var categoryElmtAncr = document.createElement("a");
+    categoryElmtAncr.innerHTML = targetDate + targetName;
+    categoryElmtAncr.href = "./loader.html?" + targetUrlSection + "=" + targetUrl;
+    categoryElmtPara.appendChild(categoryElmtAncr);
+    return categoryElmtPara;
 }
 
 function parseCategoryPage(urlRequest, pageRequest){
@@ -51,7 +62,7 @@ function parseCategoryPage(urlRequest, pageRequest){
         mainContent.appendChild(pageTitle);
         for(var i=0; i<arrCategory.length; i++){
             mainContent.appendChild(
-                CategoryPageWrapper(
+                categoryPageWrapper(
                     arrCategoryL[i],
                     "category",
                     "",
@@ -65,7 +76,7 @@ function parseCategoryPage(urlRequest, pageRequest){
         mainContent.appendChild(pageTitle);
         for(var i=0; i<arrCategory.length; i++){
             mainContent.appendChild(
-                CategoryPageWrapper(
+                categoryPageWrapper(
                     arrCategoryL[i],
                     "post",
                     getTitleDate(arrCategory[i]) + " - ",
@@ -74,5 +85,5 @@ function parseCategoryPage(urlRequest, pageRequest){
             );
         }
     }
-    document.getElementById("main-content").innerHTML = pageHTML;
+    // document.getElementById("main-content").innerHTML = pageHTML;
 }
