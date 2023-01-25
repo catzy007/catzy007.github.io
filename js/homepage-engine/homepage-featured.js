e989119e8f5976344c34b246f3713ed1babdc8d6
catzy007 <catzy.rais@gmail.com>
2023-01-25 08:25:27 +0700

    bump to BS5, drop jquery, drop IE11 support

--- js/homepage-engine/homepage-featured.js
+++ js/homepage-engine/homepage-featured.js
@@ -2,9 +2,16 @@ function loadHomepageFeatured(){
     var text = this.responseText;
     var featured = text
     var featuredLower = text.toLowerCase();
+    //desktop featured
     document.getElementById("featuredPostCard").onclick = function () { location.href='./loader.html?post=' + featuredLower };
     setElmtThumbnail("featuredPostImg", "./posts/" + featuredLower + "/thumbnail.jpg");
     document.getElementById("featuredPostDate").innerHTML = getTitleDate(featured);
     document.getElementById("featuredPostTitle").innerHTML = getTitleOnly(featured);
     document.getElementById("featuredPostCard").style.display = 'block';
+    //mobile featured
+    document.getElementById("featuredPostCardM").onclick = function () { location.href='./loader.html?post=' + featuredLower };
+    setElmtThumbnail("featuredPostImgM", "./posts/" + featuredLower + "/thumbnail.jpg");
+    document.getElementById("featuredPostDateM").innerHTML = getTitleDate(featured);
+    document.getElementById("featuredPostTitleM").innerHTML = getTitleOnly(featured);
+    document.getElementById("featuredPostCardM").style.display = 'block';
 }
